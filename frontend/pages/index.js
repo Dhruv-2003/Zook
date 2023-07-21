19f2b4be6b5468e44efbdfc5fb8ec4b24c83e0a2
Architsharma7 <architsharma7911@gmail.com>
2023-07-21 21:06:16 +0200

    feat: xmtp notifications

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,10 @@
 import { Web3Button } from "@web3modal/react";
+import Notifications from "../components/notifications";
+import { useAccount } from "wagmi";
 
 export default function Home() {
-
+  const { isConnected } = useAccount();
   return (
-    <div className="">
-      <Web3Button />
-    </div>
+    <div className="">{!isConnected ? <Web3Button /> : <Notifications />}</div>
   );
 }
