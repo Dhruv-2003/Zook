7b5ada82135c3ce1255b76d2ca08ea8b8f76f54c
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-07-22 14:43:24 +0200

    fix: changes

--- frontend/components/eas.js
+++ frontend/components/eas.js
@@ -6,6 +6,7 @@ import {
 } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
 
+// https://sepolia.easscan.org/offchain/url/#attestation=eNqlkj2OFEEMRu%2FS8Qj5v%2Bx0ZuESiKDsqjoAAonj41kikBZptQ46sp9ff66vF3wiu26IqNqf2wW%2FHmQjzv2Fx1Jh98eW4C%2F4ch%2FKBC%2FEttFR9%2FVsZok5lpDWyXUct2AoWyorzam%2BoEBtZKnLcIRQam74RJ3se75CaBnupElKOX2v6RG1wBijcT0ylh%2BbvNrk2ZbqTp5ueYidz3Ujf3KMHsJMLL0qYlYaPlq1rVjQPIA%2FA%2FP9vC4FCz24d46ofWrt02oH2HPwEKlqlSGmE50nJ0zkU0yFAHpqxx%2FIMB20hmG0KIxT1YTJu4rNonenxtki4Ed1ybBJ%2FT9pLh54%2FHqfNloAkDjoDXryuv34%2FnO%2FerxRRvUvuv5G70bneWveoy%2B4cqkrbHDbVhCLqiMhy%2BwAz%2FFihvfW2pCWPIfJ%2F9oivWODvZL7dXZk02bnHzQ4QKjPMaTf4oKPFX7U%2F%2Bp74LffPnHFzw%3D%3D
 // https://sepolia.easscan.org/schema/view/0x90d11fc17e957469d3d5a752c60d71f98a8ff98a153ff3e45025716cbd90d340
 // https://sepolia.easscan.org/schema/view/0x076572d761919907fcc038a3ecc3669818b59fe4408f55d476a2952b684891f8
 
@@ -27,7 +28,6 @@ const EAS_CONFIG = {
   address: EASContractAddress,
   version: EASVersion, // 0.26
   chainId: CHAINID,
-
 };
 
 class EASService {
@@ -89,9 +89,9 @@ class EASService {
     ]); // the value of the encoded data can be chosen by the inputs we want it to be
 
     const address = await this.signer.getAddress();
-    console.log(address)
-    console.log(this.signer)
-    console.log(this.easClient)
+    console.log(address);
+    console.log(this.signer);
+    console.log(this.easClient);
 
     const tx = await this.easClient.attest({
       schema: SchemaUID,
@@ -117,7 +117,7 @@ class EASService {
     totalOwed
   ) {
     const timestamp = Math.floor(Date.now() / 1000);
-    console.log(timestamp)
+    console.log(timestamp);
     const address = await this.signer.getAddress();
     const schemaEncoder = new SchemaEncoder(rawSchema);
 
@@ -129,8 +129,8 @@ class EASService {
       { name: "ChannelID", value: channelId, type: "uint16" },
       { name: "TotalOwed", value: totalOwed, type: "uint256" },
     ]);
-    console.log(encodedData)
-    const offChainClient= new Offchain(EAS_CONFIG, 1);
+    console.log(encodedData);
+    const offChainClient = new Offchain(EAS_CONFIG, 1);
 
     const offchainAttestation = await offChainClient.signOffchainAttestation(
       {
@@ -142,7 +142,7 @@ class EASService {
         revocable: true,
         nonce: 0,
         schema: SchemaUID,
-        version: 1, 
+        version: 1,
         refUID:
           "0x0000000000000000000000000000000000000000000000000000000000000000",
         data: encodedData,
@@ -155,4 +155,4 @@ class EASService {
   }
 }
 
-export default EASService
+export default EASService;
